944f840d3fee4d8f14791199e1caa344e422bc05
Samuel Ytterbrink <say@praqma.net>
2017-11-06 17:16:37 +0100

    Add glob and fromat to git for-each-ref

--- git_metrics/git.py
+++ git_metrics/git.py
@@ -1,4 +1,8 @@
 
 
-def for_each_ref():
-    return "git for-each-ref"
+def for_each_ref(glob=None, format=None):
+    return sum((
+        ["git", "for-each-ref"],
+        [] if format is None else [f"--format={format}"],
+        [] if glob is None else [glob],
+    ), [])
--- git_metrics/test_for_each_ref.py
+++ git_metrics/test_for_each_ref.py
@@ -1,5 +1,21 @@
-from .git import for_each_ref
+from git_metrics.git import for_each_ref
 
 
 def test_normal():
-    assert for_each_ref() == "git for-each-ref"
+    assert for_each_ref() == ["git", "for-each-ref"]
+
+
+def test_ref_glob():
+    assert for_each_ref('refs/heads/*') == [
+        "git",
+        "for-each-ref",
+        "refs/heads/*"
+    ]
+
+
+def test_format():
+    assert for_each_ref(format='%(authordate:unix)') == [
+        "git",
+        "for-each-ref",
+        "--format=%(authordate:unix)"
+    ]
