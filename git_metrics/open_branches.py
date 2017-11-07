c40cb7b70e98ca2536c6c1d6aa3bc9e32ea693d4
Samuel Ytterbrink <say@praqma.net>
2017-11-07 10:18:20 +0100

    Set universal_newlines to True so that we don't need to decode byte strings.

--- git_metrics/open_branches.py
+++ git_metrics/open_branches.py
@@ -12,17 +12,17 @@ def run(cmd):
     return subprocess.Popen(
         cmd,
         stdout=subprocess.PIPE,
-        cwd=cwd
+        cwd=cwd,
+        universal_newlines=True
     )
 
 
 now = int(time.time())
 get_refs = for_each_ref('refs/remotes/origin/*', format='%(refname) %(authordate:unix)')
-
-with run(get_refs) as pipe:
-    for branch, t in columns(decode(pipe.stdout)):
+with run(get_refs) as program:
+    for branch, t in columns(program.stdout):
         get_time = log(f"origin/master..{branch}", format='%at')
-        with run(get_time) as inner_pipe:
-            for author_time, in columns(decode(inner_pipe.stdout)):
+        with run(get_time) as inner_program:
+            for author_time, in columns(inner_program.stdout):
                 print(f"{now - int(author_time)}, {branch}")
 
