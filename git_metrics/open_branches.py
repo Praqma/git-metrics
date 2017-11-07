a3ea009f307a251210e57ecec5dc6198b4d5bbc5
Samuel Ytterbrink <say@praqma.net>
2017-11-07 10:30:37 +0100

    Add simple command line help, somewhere to improve.

--- git_metrics/open_branches.py
+++ git_metrics/open_branches.py
@@ -1,28 +1,41 @@
-import subprocess
+"""Calculate age of commits in open remote branches
 
-import time
+Usage:
+    open_branches.py <path_to_git_repo>
+    open_branches.py (-h | --help)
+"""
+
 import sys
+import time
+from subprocess import Popen, PIPE
+from functools import partial
+
+import docopt
 
 from git_metrics.git import for_each_ref, log
 from git_metrics.parser import columns
 cwd = sys.argv[1]
 
 
-def run(cmd):
-    return subprocess.Popen(
-        cmd,
-        stdout=subprocess.PIPE,
-        cwd=cwd,
+
+
+
+def main(run):
+    now = int(time.time())
+    get_refs = for_each_ref('refs/remotes/origin/**', format='%(refname) %(authordate:unix)')
+    with run(get_refs) as program:
+        for branch, t in columns(program.stdout):
+            get_time = log(f"origin/master..{branch}", format='%at')
+            with run(get_time) as inner_program:
+                for author_time, in columns(inner_program.stdout):
+                    print(f"{now - int(author_time)}, {branch}")
+
+
+if __name__ == "__main__":
+    arguments = docopt.docopt(__doc__)
+    main(partial(
+        Popen,
+        stdout=PIPE,
+        cwd=arguments['<path_to_git_repo>'],
         universal_newlines=True
-    )
-
-
-now = int(time.time())
-get_refs = for_each_ref('refs/remotes/origin/**', format='%(refname) %(authordate:unix)')
-with run(get_refs) as program:
-    for branch, t in columns(program.stdout):
-        get_time = log(f"origin/master..{branch}", format='%at')
-        with run(get_time) as inner_program:
-            for author_time, in columns(inner_program.stdout):
-                print(f"{now - int(author_time)}, {branch}")
-
+    ))
