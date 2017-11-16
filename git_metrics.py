22dc350794952b6349e166d655e5f442024ea4de
Samuel Ytterbrink <say@praqma.net>
2017-11-16 12:52:39 +0100

    Add feature plot open branches from csv file.

--- git_metrics.py
+++ git_metrics.py
@@ -6,6 +6,7 @@ Usage:
     git_metrics.py open-branches [--master-branch=<branch>] --elastic=<elastic_url> --index=<elastic_index> <path_to_git_repo>
     git_metrics.py release-lead-time [--tag-pattern=<fn_match>] <path_to_git_repo>
     git_metrics.py release-lead-time --plot [--tag-pattern=<fn_match>] <path_to_git_repo>
+    git_metrics.py plot --open-branches <csv_file>
     git_metrics.py (-h | --help)
 
     Options:
@@ -15,7 +16,8 @@ import time
 from fnmatch import fnmatch
 from subprocess import Popen, PIPE
 from functools import partial
-import csv, os
+import csv
+import os
 
 import docopt
 import sys
@@ -150,50 +152,56 @@ def get_branches(run):
 
 if __name__ == "__main__":
     flags = docopt.docopt(__doc__)
-    path_to_git_repo = flags['<path_to_git_repo>']
-    repo_name = os.path.basename(os.path.abspath(path_to_git_repo))
-    run = partial(
-        Popen,
-        stdout=PIPE,
-        cwd=path_to_git_repo,
-        universal_newlines=True
-    )
-    if flags["open-branches"]:
-        master_branch = flags['--master-branch'] or 'origin/master'
-        branches = list(get_branches(run))
-        if not any(branch.endswith(master_branch) for branch in branches):
-            error = partial(print, file=sys.stderr)
-            error(f"branch {master_branch} does not exist")
-            error()
-            error(f"try one of:")
-            for branch in branches:
-                error(branch)
-            error()
-            error("use -h for more help")
-            exit(1)
-        gen = commit_author_time_and_branch_ref(run, master_branch)
-        if flags['--plot']:
-            plot_open_branches_metrics(gen, repo_name)
-        elif flags['--elastic']:
-            send_open_branches_metrics_to_elastic(
-                gen,
-                flags['--elastic'],
-                flags['--index']
-            )
-        else:
-            print_open_branches_metrics(gen)
-    else:
-        pattern = flags['--tag-pattern'] or '*'
-        data = commit_author_time_tag_author_time_and_from_to_tag_name(
-            run,
-            partial(fnmatch, pat=pattern)
+    if flags['<path_to_git_repo>']:
+        path_to_git_repo = flags['<path_to_git_repo>']
+        repo_name = os.path.basename(os.path.abspath(path_to_git_repo))
+        run = partial(
+            Popen,
+            stdout=PIPE,
+            cwd=path_to_git_repo,
+            universal_newlines=True
         )
-        if flags['--plot']:
-            plot_tags(
-                data
+        if flags["open-branches"]:
+            master_branch = flags['--master-branch'] or 'origin/master'
+            branches = list(get_branches(run))
+            if not any(branch.endswith(master_branch) for branch in branches):
+                error = partial(print, file=sys.stderr)
+                error(f"branch {master_branch} does not exist")
+                error()
+                error(f"try one of:")
+                for branch in branches:
+                    error(branch)
+                error()
+                error("use -h for more help")
+                exit(1)
+            gen = commit_author_time_and_branch_ref(run, master_branch)
+            if flags['--plot']:
+                plot_open_branches_metrics(gen, repo_name)
+            elif flags['--elastic']:
+                send_open_branches_metrics_to_elastic(
+                    gen,
+                    flags['--elastic'],
+                    flags['--index']
+                )
+            else:
+                print_open_branches_metrics(gen)
+        elif flags["release-lead-time"]:
+            pattern = flags['--tag-pattern'] or '*'
+            data = commit_author_time_tag_author_time_and_from_to_tag_name(
+                run,
+                partial(fnmatch, pat=pattern)
             )
-        else:
-            writer = csv.writer(sys.stdout, delimiter=',')
-            writer.writerows(
-                data
-            )
+            if flags['--plot']:
+                plot_tags(
+                    data
+                )
+            else:
+                writer = csv.writer(sys.stdout, delimiter=',')
+                writer.writerows(
+                    data
+                )
+    if flags["plot"]:
+        if flags["--open-branches"]:
+            with open(flags["<csv_file>"]) as csv_file:
+                gen = ((int(time), branch) for time, branch in csv.reader(csv_file, delimiter=','))
+                plot_open_branches_metrics(gen, flags["<csv_file>"])
