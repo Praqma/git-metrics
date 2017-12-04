02cccd74c689fd67578fb8674b5f26f008b7291f
Samuel Ytterbrink <say@praqma.net>
2017-12-04 13:58:15 +0100

    Add function to zip iterable with its own tail.

--- data.py
+++ data.py
@@ -1,5 +1,16 @@
+from itertools import tee, islice
+from typing import Iterable, TypeVar, Tuple
 
 
 def columns(iterable):
     for line in iterable:
         yield line.split()
+
+
+T = TypeVar('T')
+
+
+def zip_with_tail(to_zip: Iterable[T]) -> Iterable[Tuple[T, T]]:
+    a, b = tee(to_zip)
+    tail = islice(b, 1, None)
+    return zip(a, tail)
--- test_data.py
+++ test_data.py
@@ -1,4 +1,5 @@
 from data import columns
+from data import zip_with_tail
 
 
 def test_single_column():
@@ -7,3 +8,7 @@ def test_single_column():
 
 def test_two_columns():
     assert list(columns(["a c", "b d"])) == [["a", "c"], ["b", "d"]]
+
+
+def test_zip_with_tail():
+    assert [(0, 1), (1, 2)] == list(zip_with_tail(range(3)))
