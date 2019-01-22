3fafe88b38196d1e0e42dfbe756869bce9a91301
Joel Setterberg <joel.setterberg.2@tacdis.com>
2019-01-22 17:16:16 +0100

    Started recovery time calculations

--- calculate_four_metrics.py
+++ calculate_four_metrics.py
@@ -8,6 +8,7 @@ Usage:
     calculate_four_metrics.py lead-time [--deploy-tag-pattern=<fn_match>] [--start-date=<timestamp>] <path_to_git_repo>
     calculate_four_metrics.py deploy-interval [--deploy-tag-pattern=<fn_match>] [--start-date=<timestamp>] <path_to_git_repo>
     calculate_four_metrics.py change-fail-rate [--deploy-tag-pattern=<fn_match>] [--patch-tag-pattern=<fn_match>] [--start-date=<timestamp>] <path_to_git_repo>
+    calculate_four_metrics.py recovery-time [--deploy-tag-pattern=<fn_match>] [--patch-tag-pattern=<fn_match>] [--start-date=<timestamp>] <path_to_git_repo>
     calculate_four_metrics.py (-h | --help)
 
     Options:
@@ -73,8 +74,21 @@ def main():
                 start_date,
             )
             change_fail_rate = len(list(patch_tags)) / len(list(deploy_tags)) * 100
-
             print(f"Change failure rate: {change_fail_rate:.1f}%")
+        if flags["recovery-time"]:
+            start_date = int(flags["--start-date"] or 0)
+            deploy_pattern = flags['--deploy-tag-pattern'] or '*'
+            patch_pattern = flags['--patch-tag-pattern'] or '*'
+            deploy_tags = fetch_tags_and_dates(
+                run,
+                partial(fnmatch, pat=deploy_pattern),
+                start_date,
+            )
+            patch_tags = fetch_tags_and_dates(
+                run,
+                partial(fnmatch, pat=patch_pattern),
+                start_date,
+            )
 
 
 if __name__ == "__main__":
---
+++ test_recovery_time.py
@@ -0,0 +1,87 @@
+from dataclasses import dataclass
+
+
+def find_outages(deployments):
+    results = []
+
+    for patch_split in split_sequence(deployments):
+        first_patch = patch_split[0]
+
+        deployment_index = deployments.index(first_patch) - 1
+        if deployment_index >= 0:
+            failed_deployment = deployments[deployment_index]
+            last_patch = patch_split[-1]
+            results.append((failed_deployment, last_patch))
+
+    return results
+
+
+def split_sequence(deployments):
+    split = []
+    for deployment in deployments:
+        if deployment.is_patch:
+            split.append(deployment)
+        else:
+            if split:
+                yield split
+            split = []
+
+    if split:
+        yield split
+
+@dataclass
+class Deployment:
+    is_patch: bool
+    time: int
+
+
+deployment_zero = Deployment(False, 0)
+deployment_two = Deployment(False, 2)
+patch_one = Deployment(True, 1)
+patch_three = Deployment(True, 3)
+patch_four = Deployment(True, 4)
+
+
+def test_empty_tags_gives_empty_recoveries():
+    results = find_outages([])
+    assert results == []
+
+
+def test_one_simple_outage():
+    results = find_outages([deployment_zero, patch_one])
+    assert results == [(deployment_zero, patch_one)]
+
+
+def test_one_outage_two_patches():
+    patch_two = Deployment(True, 2)
+    results = find_outages([deployment_zero, patch_one, patch_two])
+    assert results == [(deployment_zero, patch_two)]
+
+
+def test_split_sequence():
+    results = split_sequence([deployment_zero, patch_one, deployment_two, patch_three, patch_four])
+    assert list(results) == [[patch_one], [patch_three, patch_four]]
+
+
+def test_split_sequence_muliple_successed():
+    deployment_five = Deployment(False, 5)
+    patch_six = Deployment(True, 6)
+    results = split_sequence([deployment_zero, patch_one, deployment_two, deployment_five, patch_six])
+    assert list(results) == [[patch_one], [patch_six]]
+
+
+def test_split_sequence_start_with_patch():
+    deployment_five = Deployment(False, 5)
+    results = split_sequence([patch_one, deployment_two, deployment_five])
+    assert list(results) == [[patch_one]]
+
+
+def test_two_outage_two_patches():
+    results = find_outages([deployment_zero, patch_one, deployment_two, patch_three])
+    assert results == [(deployment_zero, patch_one), (deployment_two, patch_three)]
+
+
+def test_begin_with_patche():
+    results = find_outages([patch_one, deployment_two, patch_three])
+    assert results == [(deployment_two, patch_three)]
+
